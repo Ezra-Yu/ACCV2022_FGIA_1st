244c6bf064f3ce54b978eed10c520abe99b1f3df
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-09-30 21:37:18 +0800

    add slurm tool

--- tools/slurm_infer.sh
+++ tools/slurm_infer.sh
@@ -2,10 +2,26 @@
 
 export PYTHONPATH=`pwd`:$PYTHONPATH 
 
+set -x
+
 PARTITION=$1
 JOB_NAME=$2
 CONFIG=$3
 CHECKPOINT=$4
+FOLDER=$5
+GPUS=${GPUS:-8}
+GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+CPUS_PER_TASK=${CPUS_PER_TASK:-5}
+PY_ARGS=${@:5}
+SRUN_ARGS=${SRUN_ARGS:-""}
 
-srun -p ${PARTITION} --job-name=${JOB_NAME} --gres=gpu:1 --ntasks=1 --ntasks-per-node=1 --cpus-per-task=5 \ 
-    python -u tools/infer.py ${CONFIG} ${CHECKPOINT} ./data/ACCV_workshop/test --out-keys filename pred_class --out result.csv
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+srun -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --kill-on-bad-exit=1 \ 
+    ${SRUN_ARGS} \
+    python -u tools/infer_folder.py ${CONFIG} ${CHECKPOINT} ${FOLDER} --out-keys filename pred_class --out result.csv --launcher="slurm" ${PY_ARGS}
