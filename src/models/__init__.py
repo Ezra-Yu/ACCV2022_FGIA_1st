b8bde04bb9ff9c841224084fb5ddb62bc5cc27d3
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-10-25 16:24:16 +0800

    add reduction

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from .arcface_head import ArcFaceClsHead
 from .efficientnet import TimmEfficientNet
 from .tta_classifier import TTAImageClassifier
 from .linear_head_lt import LinearClsHeadWithAdjustment
+from .reduction import LinearReduction
