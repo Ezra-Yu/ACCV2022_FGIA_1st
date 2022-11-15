1625520d97873653f1d2d660ddbd8c2ca98ff587
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-11-15 17:04:52 +0800

    update

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from .arcface_head import ArcFaceClsHead
-from .efficientnet import TimmEfficientNet
 from .tta_classifier import TTAImageClassifier
 from .linear_head_lt import LinearClsHeadWithAdjustment
 from .reduction import LinearReduction
